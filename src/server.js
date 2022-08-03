39ffc58bd90883ad96f9ced8db0f6d82106b5962
Harrison Katz <git@hkatz.dev>
2022-08-03 08:13:35 -0700

    feat: Add process seperation to XSS detection
    This will allow for better sandboxing and quicker restarts for mischievous code causing crashes.

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import bodyParser from "koa-bodyparser";
 import Router from "@koa/router";
 import genWeb from "@srnd/codecup-genericwebsite";
 import vulnerableHTMLSnippet from "./vulnerableHTMLSnippet.js";
-import detectXSS from "./detectXSS.js";
+import { fork } from "child_process";
 
 const flag = process.env.FLAG || 'test';
 const seed = process.env.SEED || flag;
@@ -13,21 +13,42 @@ const app = new Koa();
 const router = new Router();
 const port = process.env.PORT || 8080;
 const tpl = genWeb.randomTemplate(seed);
+let requestID = 0n;
+
+const launchXSSDetect = () => {
+	return fork("detectXSSChild.js");
+};
+let child = launchXSSDetect();
+
+const runXSSDetect = (body, flag) => {
+	return new Promise((resolve) => {
+		const currID = (requestID++).toString();
+		const flagRecieved = (flagInfo) => {
+			if (flagInfo.requestID === currID) {
+				child.off("message", flagRecieved);
+				resolve(flagInfo.exploited);
+			}
+		}
+		child.on("message", flagRecieved);
+		child.send({body: body, flag: flag, requestID: currID});
+	});
+};
+
 app.use(bodyParser());
 
-router.get('/', (ctx) => {
+router.get('/', async (ctx) => {
 	ctx.body = tpl('Login', vulnerableHTMLSnippet("user"));
 });
 
-router.get('/template', (ctx) => {
+router.get('/template', async (ctx) => {
 	ctx.body = tpl('hint',fs.readFileSync(path.join(process.cwd(), "/public", 'template.html'), 'utf8')); // idk if this is needed
 });
 
-router.post('/', (ctx) => {
+router.post('/', async (ctx) => {
 	ctx.body = tpl('Login', vulnerableHTMLSnippet(ctx.request.body.search));
 
 	// TODO: sandbox
-	if (detectXSS(ctx.body, flag)) {
+	if (await runXSSDetect(ctx.body, flag)) {
 		ctx.cookies.set("flag", flag, {secure: false, httpOnly: false, overwrite: true});
 	}
 });
