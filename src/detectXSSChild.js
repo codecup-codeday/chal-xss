8fa039250b922e7ff74d63a854bc51d625de63c0
Harrison Katz <git@hkatz.dev>
2022-08-03 23:15:47 -0700

    feat!: Add time limit This prevents the user from permanently hanging the program. Currently set at a 5 second soft limit and 7.5 hard limit.

--- src/detectXSSChild.js
+++ src/detectXSSChild.js
@@ -7,5 +7,11 @@ process.on("message", (data) => {
 			external: true,
 		}
 	});
-	process.send({exploited: vm.run(`const {detectXSS} = require("./detectXSS.cjs"); return detectXSS('${data.body.replaceAll(/\n/g, "\\n").replaceAll(/"/g, "\\'")}', "${data.flag}")`), requestID: data.requestID});
+	let flagInfo = {requestID: data.requestID};
+	try {
+		flagInfo.exploited = vm.run(`const {detectXSS} = require("./detectXSS.cjs"); return detectXSS('${data.body.replaceAll(/\n/g, "\\n").replaceAll(/"/g, "\\'")}', "${data.flag}")`);
+	} catch (err) {
+		flagInfo.error = err.message;
+	}
+	process.send(flagInfo);
 });
