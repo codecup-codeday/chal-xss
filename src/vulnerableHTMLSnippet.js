47cec30fbee81bb1388e4232df22f24389d537fb
Jeff Li <jeffthegreat66@gmail.com>
2022-08-04 05:50:56 -0700

    feat: Improve hint

--- src/vulnerableHTMLSnippet.js
+++ src/vulnerableHTMLSnippet.js
@@ -5,6 +5,9 @@ export default function vulnerableHTMLSnippet(user) {
 					<input type = "submit" value="Search" />
 				</form>
 				<footer>
-					<p><a href="/template">hint</a></p>
+					<p><a href="/template">Hint 1</a></p>
+					<!-- The following script is MIT Licensed by Tristan Edwards (see https://unpkg.com/sweetalert@2.1.2/LICENSE.md) -->
+					<script src="https://unpkg.com/sweetalert@2.1.2/dist/sweetalert.min.js"></script>
+ 					<button onclick='swal( "Hint 2" ,  "Someone took away our admin privileges! \\n They left us a Xtremely Sus Site and a empty bag of Chips Ahoy. \\n ARRRGG!" )'>Hint 2</button>
 				</footer>`
 }
